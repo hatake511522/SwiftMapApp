e93157dec9af384e0faf1c6bb69b2a16fe9f7349
hatake511522 <623dgk511522@gmail.com>
2020-01-26 20:35:33 +0900

    Initial Commit

---
+++ MyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by 畑洋樹 on 2020/01/26.
+//  Copyright © 2020 畑洋樹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
